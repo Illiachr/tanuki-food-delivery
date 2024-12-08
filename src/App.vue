350f0a774220f92b46d8a9233f6e21c84d11db31
Illiachr <74302872+Illiachr@users.noreply.github.com>
2024-12-08 15:05:46 +0100

    feat: vue js (#5)
    vue js was introduced to the project
    
    home page was decomposed to components
    
    implemented events to toggle popup
    
    introduced vue router
    
    implemented card rendering from data

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Popup from './components/Popup.vue';
+
+const isOpen = ref(false);
+</script>
+
+<template>
+  <Header @togglePopup="isOpen = true"/>
+  <main class="main">
+    <router-view></router-view>
+  </main>
+  <Popup :open="isOpen" @togglePopup="isOpen = false" />
+  <Footer />
+</template>
+
+<style scoped></style>
---
+++ src/components/Promo.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section promo">
+      <div class="container">
+        <div class="promo__inner">
+          <div class="promo--text">
+            <h1 class="promo--title">Online food delivery service</h1>
+            <p class="promo--subtitle">Your favorite restaurant's dishes delivered by a masked, gloved courier with sanitizer</p>
+          </div>
+          <div class="promo--cover"></div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
